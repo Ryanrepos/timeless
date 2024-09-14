7fc727ea970416fc6604adc91f84c092b63bea38
ryanrepos <mamarasulov911@gmail.com>
2024-09-15 02:41:26 +0900

    feat: develop subsribe, unsubscribe, getMemberFollowings, getMemberFollowers GraphQL APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -10,11 +10,8 @@ export const availablePropertySorts = [
 	'propertyViews',
 	'propertyRank',
 	'propertyPrice',
-]; 
-export const availableCommentSorts = [
-	'createdAt',
-	'updatedAt'
-]
+];
+export const availableCommentSorts = ['createdAt', 'updatedAt'];
 
 export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
 
@@ -38,5 +35,23 @@ export const lookupMember = {
 		localField: 'memberId',
 		foreignField: '_id',
 		as: 'memberData',
+	},
+};
+
+export const lookupFollowingData = {
+	$lookup: {
+		from: 'members',
+		localField: 'followingId',
+		foreignField: '_id',
+		as: 'followingData',
+	},
+};
+
+export const lookupFollowerData = {
+	$lookup: {
+		from: 'members',
+		localField: 'followerId',
+		foreignField: '_id',
+		as: 'followerData',
 	},
 };
